d36ea743532b31944f14b430f619e94cd714effb
Cengiiz <dadem64@gmail.com>
2021-05-04 13:19:05 +0300

    Update kripto.c

--- src/kripto.c
+++ src/kripto.c
@@ -1,45 +1,49 @@
 #include <fcntl.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "fields.h"
 #include <cJSON.h>
+#include "jrb.h"
 
-void encrypt(IS is_input);
+void encrypt();
 void decrypt();
 
 int main(int argc, char **argv)
 {
-  IS is_input;
-  is_input = new_inputstruct("data/istiklal_marsi/istiklal_marsi.txt");
-
-  encrypt(is_input);
+  // encrypt();
   decrypt();
 
   return 0;
 }
 
-void decrypt() 
+void decrypt()
 {
-  FILE *f = fopen ("data/istiklal_marsi/decrypted_test.txt", "w+");
+  return;
 }
 
-void encrypt(IS is_input)
+void encrypt()
 {
+  IS is_input;
   FILE *fp;
   int i, j;
   char *buffer = 0;
   long length;
-  FILE *f = fopen ("data/istiklal_marsi/.kilit", "rb");
+  FILE *f = fopen ("data/istiklal_marsi/.kilit", "r");
   cJSON *json;
   cJSON *name = NULL;
+  JRB b, tmp;
+  b = make_jrb();
+  tmp = make_jrb();
+  is_input = new_inputstruct("data/istiklal_marsi/istiklal_marsi.txt");
 
   if (is_input == NULL) {
     perror("Hata: ");
     exit(1);
   }
 
-  fp = fopen("data/istiklal_marsi/encripted_test", "w+");
-  if (fp < 0) { perror("data/hata.txt"); exit(1); }
+  fp = fopen("data/istiklal_marsi/encrypted_test", "w+");
+  if (fp < 0) { perror("Error: "); exit(1); }
 
   if (f)
   {
@@ -57,17 +61,29 @@ void encrypt(IS is_input)
   if (buffer)
   {
     json = cJSON_Parse(buffer);
+    cJSON *current_element = NULL;
+    char *current_key = NULL;
+
+    cJSON_ArrayForEach(current_element, json)
+    {
+        current_key = current_element->string;
+        if (current_key != NULL)
+        {
+            // printf("%s ", current_element->valuestring);
+            (void) jrb_insert_str(b, strdup(current_key), new_jval_v(current_element->valuestring));
+        }
+    }
+
     while(get_line(is_input) >= 0) {
       for (i = 0; i < is_input->NF; i++) {
-        name = cJSON_GetObjectItemCaseSensitive(json, is_input->fields[i]);
-        fprintf(fp, "%s ", name->valuestring);
-        // printf("%d: %s: ", is_input->line, is_input->fields[i]);
-        // printf("%s\n", name->valuestring);
+        tmp = jrb_find_str(b, is_input->fields[i]);
+        fprintf(fp, "%s ", tmp->val.s);
       }
     }
   }
 
   jettison_inputstruct(is_input);
   fclose(fp);
+  jrb_free_tree(b);
   return;
 }
