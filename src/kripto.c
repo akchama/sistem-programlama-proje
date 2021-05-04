70a0b563ca8ac305f3968cb64ff5f37cd80a2c30
bsrkynk <busra.kaynak@ogr.sakarya.edu.tr>
2021-05-04 13:34:25 +0300

    dosya içinde ki json objelerinin JRB ağacına eklenmesi (Descryption)

--- src/kripto.c
+++ src/kripto.c
@@ -19,7 +19,52 @@ int main(int argc, char **argv)
 
 void decrypt()
 {
-  return;
+    FILE* fp;
+    int i, j;
+    char* buffer = 0;
+    char* str = 0;
+    long length;
+    FILE* f = fopen("data/istiklal_marsi/.kilit", "rb");
+    cJSON* json;
+    JRB b, tmp;
+    b = make_jrb();
+    tmp = make_jrb();
+
+    fp = fopen("data/istiklal_marsi/decrypted_test.txt", "w+");
+    if (fp < 0) { perror("Error: "); exit(1); }
+
+    if (f)
+    {
+        fseek(f, 0, SEEK_END);
+        length = ftell(f);
+        fseek(f, 0, SEEK_SET);
+        buffer = malloc(length);
+        if (buffer)
+        {
+            fread(buffer, 1, length, f);
+        }
+        fclose(f);
+    }
+
+    if (buffer)
+    {
+        json = cJSON_Parse(buffer);
+        cJSON* current_element = NULL;
+        char* current_key = NULL;
+
+        cJSON_ArrayForEach(current_element, json)
+        {
+            current_key = current_element->string;
+            if (current_key != NULL)
+            {
+                // printf("%s ", current_element->valuestring);
+                (void)jrb_insert_str(b, strdup(current_element->valuestring), new_jval_v(current_key));
+            }
+        }
+    }
+
+    fclose(fp);
+    return;
 }
 
 void encrypt()
